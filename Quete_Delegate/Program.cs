cbb495cc8f8ea4dee4a17a9d0df9a9013e85430e
Sebastien David <sebastien.david08@gmail.com>
2021-10-10 23:29:39 +0200

    Ajoutez des fichiers projet.

---
+++ Quete_Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quete_Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowMoney show = Dollar;
+            Console.WriteLine(show(156.32));
+            show = Euro;
+            Console.WriteLine(show(156.32));
+
+            Console.ReadLine();
+        }
+
+        public delegate string ShowMoney(double x);
+
+        public static string Dollar(double x)
+        {
+            return $" Vous avez {x} dollars";
+        }
+
+        public static string Euro(double x)
+        {
+            return $" Vous avez {x} euros";
+        }
+        
+        
+    }
+}
